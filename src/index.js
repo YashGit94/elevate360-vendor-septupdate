9d9be23675ef80b64764e46c0d5f3030fa823a01
YashGit94 <94968927+YashGit94@users.noreply.github.com>
2026-02-04 13:26:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -92,6 +92,6 @@ app.get('/api/escalation-rate', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
